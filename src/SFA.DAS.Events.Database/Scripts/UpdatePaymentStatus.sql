b7ffeb5d6034cedbeeef32d28340a9e283755f18
Lee Wadhams <lee.wadhams@valtech.com>
2016-12-05 13:07:45 +0000

    Removed db name from scripts

--- src/SFA.DAS.Events.Database/Scripts/UpdatePaymentStatus.sql
+++ src/SFA.DAS.Events.Database/Scripts/UpdatePaymentStatus.sql
@@ -1,33 +1,32 @@
--- UPDATE PaymentStatus
 BEGIN TRANSACTION [Tran1]
 BEGIN TRY
 
 
-UPDATE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents]
+UPDATE [dbo].[ApprenticeshipEvents]
 SET [PaymentStatus]='0'
 WHERE [PaymentStatus]='PendingApproval'
 
-UPDATE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents]
+UPDATE [dbo].[ApprenticeshipEvents]
 SET [PaymentStatus]='1'
 WHERE [PaymentStatus]='Active'
 
-UPDATE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents]
+UPDATE [dbo].[ApprenticeshipEvents]
 SET [PaymentStatus]='2'
 WHERE [PaymentStatus]='Paused'
 
-UPDATE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents]
+UPDATE [dbo].[ApprenticeshipEvents]
 SET [PaymentStatus]='3'
 WHERE [PaymentStatus]='Cancelled'
 
-UPDATE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents]
+UPDATE [dbo].[ApprenticeshipEvents]
 SET [PaymentStatus]='4'
 WHERE [PaymentStatus]='Completed'
 
-UPDATE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents]
+UPDATE [dbo].[ApprenticeshipEvents]
 SET [PaymentStatus]='5'
 WHERE [PaymentStatus]='Deleted'
 
-ALTER TABLE [SFA.DAS.Events.Database].[dbo].[ApprenticeshipEvents] ALTER COLUMN PaymentStatus SMALLINT NOT NULL
+ALTER TABLE [dbo].[ApprenticeshipEvents] ALTER COLUMN PaymentStatus SMALLINT NOT NULL
 
 COMMIT TRANSACTION [Tran1]
 END TRY
