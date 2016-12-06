984e04012a0aed0d81f3b8472c4393f1b2c86e09
Lee Wadhams <lee.wadhams@valtech.com>
2016-12-06 14:13:04 +0000

    Added logging for unhandled exceptions

--- src/SFA.DAS.Events.Api/App_Start/ValidationExceptionHandler.cs
+++ src/SFA.DAS.Events.Api/App_Start/ValidationExceptionHandler.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Web.Http.ExceptionHandling;
 using FluentValidation;
+using NLog;
 
 namespace SFA.DAS.Events.Api
 {
     public class ValidationExceptionHandler : ExceptionHandler
     {
+        private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
         public override void Handle(ExceptionHandlerContext context)
         {
             if (context.Exception is ValidationException)
@@ -16,8 +18,13 @@ namespace SFA.DAS.Events.Api
                 var message = ((ValidationException)context.Exception).Message;
                 response.Content = new StringContent(message);
                 context.Result = new ValidationErrorResult(context.Request, response);
+
+                _logger.Warn(context.Exception, "Validation error");
+
                 return;
             }
+
+            _logger.Error(context.Exception, "Unhandled exception");
 
             base.Handle(context);
         }
