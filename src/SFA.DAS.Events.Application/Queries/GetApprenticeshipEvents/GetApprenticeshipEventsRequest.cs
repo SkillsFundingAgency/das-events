eb2773d25d5d1aeb5a8a73a8fa035992ab53cbe4
Mark Gwilliam <github@markgwilliam.com>
2017-01-12 15:51:45 +0000

    added base request type

--- src/SFA.DAS.Events.Application/Queries/GetApprenticeshipEvents/GetApprenticeshipEventsRequest.cs
+++ src/SFA.DAS.Events.Application/Queries/GetApprenticeshipEvents/GetApprenticeshipEventsRequest.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using MediatR;
 
 namespace SFA.DAS.Events.Application.Queries.GetApprenticeshipEvents
 {
-    public sealed class GetApprenticeshipEventsRequest : IAsyncRequest<GetApprenticeshipEventsResponse>
-    {
-        public DateTime FromDateTime { get; set; }
-        public DateTime ToDateTime { get; set; }
-        public int PageSize { get; set; }
-        public int PageNumber { get; set; }
-        public long FromEventId { get; set; }
-    }
+    public sealed class GetApprenticeshipEventsRequest : QueryRequest<GetApprenticeshipEventsResponse> {}
 }
