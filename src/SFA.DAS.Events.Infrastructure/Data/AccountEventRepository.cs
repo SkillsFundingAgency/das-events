3eaa1633fcd34bb3cae9b2110a86f0f35005e261
Mark Gwilliam <github@markgwilliam.com>
2017-01-12 15:52:17 +0000

    moved dupe methods to base repo class

--- src/SFA.DAS.Events.Infrastructure/Data/AccountEventRepository.cs
+++ src/SFA.DAS.Events.Infrastructure/Data/AccountEventRepository.cs
@@ -9,47 +9,20 @@ namespace SFA.DAS.Events.Infrastructure.Data
 {
     public class AccountEventRepository : BaseRepository, IAccountEventRepository
     {
-        private const string TableName = "AccountEvents";
+        protected override string TableName => "AccountEvents";
 
         public AccountEventRepository(string databaseConnectionString) : base(databaseConnectionString) {}
 
         public async Task Create(AccountEvent @event)
         {
             await WithConnection(async c =>
-                await c.ExecuteAsync($"INSERT INTO [dbo].[{TableName}](Event, CreatedOn, EmployerAccountId) " + 
-                $"VALUES (@event, @createdOn, @employerAccountId;", @event));
+                await c.ExecuteAsync($"INSERT INTO [dbo].[{TableName}](Event, CreatedOn, EmployerAccountId) " +
+                                     $"VALUES (@event, @createdOn, @employerAccountId;", @event));
         }
 
-        public async Task<IEnumerable<AccountEvent>> GetByDateRange(DateTime fromDate, DateTime toDate, int pageSize, int pageNumber, long fromEventId)
+        public async Task<IEnumerable<AccountEvent>> GetByRange(DateTime fromDate, DateTime toDate, int pageSize, int pageNumber, long fromEventId)
         {
-            var offset = pageSize*(pageNumber - 1);
-
-            // query by ID or by date range
-            if (fromEventId > 0)
-            {
-                return await WithConnection(async c =>
-                {
-                    var parameters = new DynamicParameters();
-                    parameters.Add("@fromEventId", fromEventId);
-
-                    var results = await c.QueryAsync<AccountEvent>($"SELECT * FROM [dbo].[{TableName}] WHERE Id >=  @fromEventId ORDER BY CreatedOn OFFSET {offset} ROWS FETCH NEXT {pageSize} ROWS ONLY;", parameters);
-
-                    return results;
-                });
-            }
-            else
-            {
-                return await WithConnection(async c =>
-                {
-                    var parameters = new DynamicParameters();
-                    parameters.Add("@fromDate", fromDate);
-                    parameters.Add("@toDate", toDate);
-
-                    var results = await c.QueryAsync<AccountEvent>($"SELECT * FROM [dbo].[{TableName}] WHERE CreatedOn >=  @fromDate AND CreatedOn < @toDate ORDER BY CreatedOn OFFSET {offset} ROWS FETCH NEXT {pageSize} ROWS ONLY;", parameters);
-
-                    return results;
-                });
-            }
+            return await GetByRange<AccountEvent>(fromDate, toDate, pageSize, pageNumber, fromEventId);
         }
     }
 }
