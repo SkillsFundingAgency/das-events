d07bf3b33559bb6c3a038204e82c10b3f1c44eb0
Michael Young <mick.young1981@googlemail.com>
2017-01-16 14:47:40 +0000

    Added CreateApprenticeshipEventCommand unit tests

---
+++ src/SFA.DAS.Events.Application.UnitTests/Builders/CreateApprenticeshipEventCommandBuilder.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using SFA.DAS.Events.Application.Commands.CreateApprenticeshipEvent;
+using SFA.DAS.Events.Domain.Entities;
+
+namespace SFA.DAS.Events.Application.UnitTests.Builders
+{
+    public class CreateApprenticeshipEventCommandBuilder
+    {
+        private AgreementStatus _agreementStatus = AgreementStatus.BothAgreed;
+        private int _apprenticeshipId = 32489;
+        private string _providerId = "ABC123";
+        private string _employerAccountId = "ZZZ999";
+        private string _event = "Test";
+        private string _learnerId = "LRNID";
+        private int _paymentOrder = 3;
+        private PaymentStatus _paymentStatus = PaymentStatus.Active;
+        private DateTime _trainingEndDate = DateTime.Now.AddYears(1);
+        private string _trainingId = "TRNID";
+        private DateTime _trainingStartDate = DateTime.Now.AddDays(-30);
+        private decimal _trainingTotalCost = 12345.67m;
+        private TrainingTypes _trainingTypes = TrainingTypes.Framework;
+
+        public CreateApprenticeshipEventCommand Build()
+        {
+            return new CreateApprenticeshipEventCommand
+            {
+                AgreementStatus = _agreementStatus,
+                ApprenticeshipId = _apprenticeshipId,
+                ProviderId = _providerId,
+                EmployerAccountId = _employerAccountId,
+                Event = _event,
+                LearnerId = _learnerId,
+                PaymentOrder = _paymentOrder,
+                PaymentStatus = _paymentStatus,
+                TrainingEndDate = _trainingEndDate,
+                TrainingId = _trainingId,
+                TrainingStartDate = _trainingStartDate,
+                TrainingTotalCost = _trainingTotalCost,
+                TrainingType = _trainingTypes
+            };
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithEvent(string @event)
+        {
+            _event = @event;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithApprenticeshipId(int apprenticeshipId)
+        {
+            _apprenticeshipId = apprenticeshipId;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithProviderId(string providerId)
+        {
+            _providerId = providerId;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithLearnerId(string learnerId)
+        {
+            _learnerId = learnerId;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithEmployerAccountId(string employerAccountId)
+        {
+            _employerAccountId = employerAccountId;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithTrainingId(string trainingId)
+        {
+            _trainingId = trainingId;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithTrainingStartDate(DateTime trainingStartDate)
+        {
+            _trainingStartDate = trainingStartDate;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithTrainingEndDate(DateTime trainingEndDate)
+        {
+            _trainingEndDate = trainingEndDate;
+            return this;
+        }
+
+        public CreateApprenticeshipEventCommandBuilder WithTrainingTotalCost(decimal trainingTotalCost)
+        {
+            _trainingTotalCost = trainingTotalCost;
+            return this;
+        }
+    }
+}
---
+++ src/SFA.DAS.Events.Application.UnitTests/CreateApprenticeshipEventTests/WhenIValidateTheCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentValidation;
+using NUnit.Framework;
+using SFA.DAS.Events.Application.UnitTests.Builders;
+
+namespace SFA.DAS.Events.Application.UnitTests.CreateApprenticeshipEventTests
+{
+    [TestFixture]
+    public class WhenIValidateTheCommand : CreateApprenticeshipEventTestBase
+    {
+        [Test]
+        public async Task AndEventIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithEvent(null).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndApprenticeshipIdIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithApprenticeshipId(0).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndProviderIdIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithProviderId(null).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndLearnerIdIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithLearnerId(null).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndEmployerAccountIdIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithEmployerAccountId(null).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndTrainingIdIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithTrainingId(null).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndTrainingStartDateIsAfterTheEndDate()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithTrainingStartDate(DateTime.Now.AddDays(-1)).WithTrainingEndDate(DateTime.Now.AddDays(-2)).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+
+        [Test]
+        public async Task AndTrainingTotalCostIsNotProvided()
+        {
+            var command = new CreateApprenticeshipEventCommandBuilder().WithTrainingTotalCost(-1).Build();
+
+            Assert.ThrowsAsync<ValidationException>(() => Handler.Handle(command));
+        }
+    }
+}
