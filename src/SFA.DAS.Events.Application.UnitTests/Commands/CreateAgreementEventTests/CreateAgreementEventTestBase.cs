87b8c3c177d80bb473733be7a6e4e6aa98f1b4d1
froberg <christofer.froberg@gmail.com>
2017-01-19 11:17:24 +0000

    Passing validator to contructor in tests

--- src/SFA.DAS.Events.Application.UnitTests/Commands/CreateAgreementEventTests/CreateAgreementEventTestBase.cs
+++ src/SFA.DAS.Events.Application.UnitTests/Commands/CreateAgreementEventTests/CreateAgreementEventTestBase.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
+
 using SFA.DAS.Events.Application.Commands.CreateAgreementEvent;
 using SFA.DAS.Events.Domain.Logging;
 using SFA.DAS.Events.Domain.Repositories;
@@ -10,15 +11,17 @@ namespace SFA.DAS.Events.Application.UnitTests.Commands.CreateAgreementEventTest
     {
         protected CreateAgreementEventCommandHandler Handler;
         protected Mock<IAgreementEventRepository> Repository;
+        protected CreateAgreementEventCommandValidator Validator;
         protected Mock<IEventsLogger> EventsLogger;
 
         [SetUp]
         public void Arrange()
         {
             Repository = new Mock<IAgreementEventRepository>();
+            Validator = new CreateAgreementEventCommandValidator();
             EventsLogger = new Mock<IEventsLogger>();
 
-            Handler = new CreateAgreementEventCommandHandler(Repository.Object, EventsLogger.Object);
+            Handler = new CreateAgreementEventCommandHandler(Repository.Object, Validator, EventsLogger.Object);
         }
     }
 }
