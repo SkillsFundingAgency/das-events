21295137997b3deea9d752d9fb046fcf167114fe
Lee Wadhams <lee.wadhams@valtech.com>
2017-02-09 14:26:48 +0000

    Create a single scripts for Pre and Post deploy db tasks

---
+++ src/SFA.DAS.Events.Database/PostDeployment/Script.MigrateEventsPost.sql
@@ -0,0 +1,35 @@
+﻿
+-- Migrate Account Events
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'AccountEvents_Temp')
+BEGIN
+	SET IDENTITY_INSERT dbo.AccountEvents ON
+
+    INSERT INTO dbo.AccountEvents
+		([Id], [Event], CreatedOn, ResourceUri)
+	SELECT [Id], 'AccountCreated', CreatedOn, '/api/accounts/' + EmployerAccountId
+	FROM dbo.AccountEvents_Temp
+
+	SET IDENTITY_INSERT dbo.AccountEvents OFF
+
+	DROP TABLE dbo.AccountEvents_Temp
+END
+
+
+-- Migrate Agreement Events
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'AgreementEvents_Temp')
+BEGIN
+	SET IDENTITY_INSERT dbo.AgreementEvents ON
+
+    INSERT INTO dbo.AgreementEvents
+		([Id], [Event], [CreatedOn], [ProviderId], [ContractType])
+	SELECT 
+		[Id], [Event], [CreatedOn], [ProviderId], 'ProviderAgreement'
+	FROM 
+		dbo.AgreementEvents_Temp
+	WHERE
+		[Event] = 'INITIATED'
+
+	SET IDENTITY_INSERT dbo.AgreementEvents OFF
+
+	DROP TABLE dbo.AgreementEvents_Temp
+END
---
+++ src/SFA.DAS.Events.Database/PreDeployment/Script.MigrateEventsPre.sql
@@ -0,0 +1,26 @@
+﻿
+-- Migrate Agreement Events
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'AgreementEvents_Temp')
+BEGIN
+	DROP TABLE dbo.AgreementEvents_Temp
+END
+
+IF EXISTS(SELECT * FROM sys.columns WHERE Name = N'EmployerAccountId' AND Object_ID = Object_ID(N'AgreementEvents'))
+BEGIN
+	SELECT * INTO AgreementEvents_Temp FROM dbo.AgreementEvents
+
+	DELETE FROM dbo.AgreementEvents
+END
+
+-- Migrate Account Events
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'AccountEvents_Temp')
+BEGIN
+	DROP TABLE dbo.AccountEvents_Temp
+END
+
+IF EXISTS(SELECT * FROM sys.columns WHERE Name = N'EmployerAccountId' AND Object_ID = Object_ID(N'AccountEvents'))
+BEGIN
+	SELECT * INTO AccountEvents_Temp FROM dbo.AccountEvents
+
+	DELETE FROM dbo.AccountEvents
+END
