23459e94f2c5a98f2b1c1bf469af5d69db16e85b
Robin O'Connor <GitHub@bytepointsoftware.com>
2017-02-17 11:31:00 +0000

    Added orchestrator for generic events controller

---
+++ src/SFA.DAS.Events.Api/Orchestrators/GenericEventOrchestrator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Events.Domain.Entities;
+
+namespace SFA.DAS.Events.Api.Orchestrators
+{
+    public class GenericEventOrchestrator : IGenericEventOrchestrator
+    {
+        public ICollection<GenericEvent> GetEvents(string eventType, string fromDate, string toDate, int pageSize, int pageNumber, long fromEventId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<GenericEvent> GetEvents(IEnumerable<string> eventTypes, string fromDate, string toDate, int pageSize, int pageNumber,
+            long fromEventId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/SFA.DAS.Events.Api/Orchestrators/IGenericEventOrchestrator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.Events.Domain.Entities;
+
+namespace SFA.DAS.Events.Api.Orchestrators
+{
+    public interface IGenericEventOrchestrator
+    {
+        ICollection<GenericEvent> GetEvents(
+          string eventType, string fromDate, string toDate, int pageSize, int pageNumber, long fromEventId);
+
+        ICollection<GenericEvent> GetEvents(
+            IEnumerable<string> eventTypes, string fromDate, string toDate, int pageSize, int pageNumber, long fromEventId);
+    }
+}
