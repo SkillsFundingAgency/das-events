ee73194c988ed740a03fda624880b1fb7799e285
Michael Young <mick.young1981@googlemail.com>
2017-04-04 08:57:14 +0100

    Removed validation around providerid, training id and total cost
    Removed validation around providerid, training id and total cost to
    align with the changes made in commitments to allow events to be created
    for all pending apprenticeships.

--- src/SFA.DAS.Events.Application/Commands/CreateApprenticeshipEvent/CreateApprenticeshipEventCommandValidator.cs
+++ src/SFA.DAS.Events.Application/Commands/CreateApprenticeshipEvent/CreateApprenticeshipEventCommandValidator.cs
@@ -8,15 +8,12 @@ namespace SFA.DAS.Events.Application.Commands.CreateApprenticeshipEvent
         {
             RuleFor(model => model.Event).NotEmpty();
             RuleFor(model => model.ApprenticeshipId).GreaterThan(0);
-            RuleFor(model => model.ProviderId).NotEmpty();
             RuleFor(model => model.LearnerId).NotEmpty();
             RuleFor(model => model.EmployerAccountId).NotEmpty();
             RuleFor(model => model.TrainingType).IsInEnum();
-            RuleFor(model => model.TrainingId).NotEmpty();
             RuleFor(model => model.TrainingStartDate).NotEmpty();
             RuleFor(model => model.TrainingEndDate).NotEmpty();
             RuleFor(start => start.TrainingStartDate).LessThanOrEqualTo(end => end.TrainingEndDate);
-            RuleFor(model => model.TrainingTotalCost).GreaterThanOrEqualTo(0);
             RuleFor(model => model.LegalEntityId).NotEmpty();
             RuleFor(model => model.LegalEntityName).NotEmpty();
             RuleFor(model => model.LegalEntityOrganisationType).NotEmpty();
