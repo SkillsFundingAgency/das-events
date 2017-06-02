414d3f8270b318f41f02ca59d99ed5bf61e7309f
Michael Young <mick.young1981@googlemail.com>
2017-06-02 09:56:02 +0100

    Added resource details to generic events.

--- src/SFA.DAS.Events.Database/Tables/GenericEvents.sql
+++ src/SFA.DAS.Events.Database/Tables/GenericEvents.sql
@@ -4,8 +4,15 @@
 	[Type] VARCHAR(255) NOT NULL,
 	[Payload] TEXT NOT NULL,
     [CreatedOn] DATETIME NOT NULL, 
+    [ResourceUri] NVARCHAR(1000) NULL, 
+    [ResourceType] NVARCHAR(255) NULL, 
+    [ResourceId] NVARCHAR(255) NULL, 
 )
 GO
 CREATE NONCLUSTERED INDEX [IX_GenericEvents_Type_Id] ON [dbo].[GenericEvents] ([Type], [Id])
 GO
 CREATE NONCLUSTERED INDEX [IX_GenericEvents_Type_CreatedOn] ON [dbo].[GenericEvents] ([Type], [CreatedOn])
+GO
+CREATE NONCLUSTERED INDEX [IX_GenericEvents_ResourceUri] ON [dbo].[GenericEvents] ([ResourceUri], [CreatedOn])
+GO
+CREATE NONCLUSTERED INDEX [IX_GenericEvents_ResourceType_ResourceId] ON [dbo].[GenericEvents] ([ResourceType], [ResourceId], [CreatedOn])
