bd645592ce2f4bf902908a6929090a17ef59f134
Michael Young <mick.young1981@googlemail.com>
2017-06-02 13:33:52 +0100

    Added query for getting generic events by resource uri.

---
+++ src/SFA.DAS.Events.Application/Queries/GetGenericEventsByResourceUri/GetGenericEventsByResourceUriQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Events.Domain.Repositories;
+
+namespace SFA.DAS.Events.Application.Queries.GetGenericEventsByResourceUri
+{
+    public class GetGenericEventsByResourceUriQueryHandler : IAsyncRequestHandler<GetGenericEventsByResourceUriRequest, GetGenericEventsByResourceUriResponse>
+    {
+        private readonly IGenericEventRepository _repository;
+
+        public GetGenericEventsByResourceUriQueryHandler(IGenericEventRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetGenericEventsByResourceUriResponse> Handle(GetGenericEventsByResourceUriRequest request)
+        {
+            var events = await _repository.GetByResourceUri(
+                request.ResourceUri, 
+                request.FromDateTime, 
+                request.ToDateTime, 
+                request.PageSize, 
+                request.PageNumber);
+
+            return new GetGenericEventsByResourceUriResponse
+            {
+                Data = events
+            };
+        }
+    }
+}
