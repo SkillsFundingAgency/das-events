90b8457716d9e4f170a0f2f1bfc88a80d00e9283
Michael Young <mick.young1981@googlemail.com>
2017-06-02 10:29:07 +0100

    Added stored procs to get generic events by resource id or uri

--- src/SFA.DAS.Events.Database/StoredProcedures/CreateGenericEvent.sql
+++ src/SFA.DAS.Events.Database/StoredProcedures/CreateGenericEvent.sql
@@ -1,6 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[CreateGenericEvent]
 	@eventType varchar(255),
-	@eventPayload text
+	@eventPayload text,
+	@resourceUri NVARCHAR(1000) = NULL,
+	@resourceType NVARCHAR(255) = NULL,
+	@resourceId NVARCHAR(255) = NULL
 AS
-	INSERT INTO [dbo].[GenericEvents] (Type, Payload, CreatedOn) 
-	VALUES (@eventType, @eventPayload, GETUTCDATE())
+	INSERT INTO [dbo].[GenericEvents] (Type, Payload, CreatedOn, ResourceUri, ResourceType, ResourceId) 
+	VALUES (@eventType, @eventPayload, GETUTCDATE(), @resourceUri, @resourceType, @resourceId)
---
+++ src/SFA.DAS.Events.Database/StoredProcedures/GetGenericEventsByResourceId.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetGenericEventsByResourceId]
+	@resourceType NVARCHAR(255),	
+	@resourceId NVARCHAR(255),	
+	@fromDate Datetime = NULL,
+	@toDate Datetime = NULL,
+	@offset int,
+	@pageSize int
+AS
+	SELECT * FROM GenericEvents ge
+	WHERE ResourceType = @resourceType AND ResourceId = @resourceId AND (@fromDate IS NULL OR CreatedOn >=  @fromDate) AND (@toDate IS NULL OR CreatedOn < @toDate)
+	ORDER BY CreatedOn OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY
