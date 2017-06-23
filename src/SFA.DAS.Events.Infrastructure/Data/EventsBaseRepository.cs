88aa2327a742270d19b1ea7bffe0adfcc4f1233b
Lee Wadhams <lee.wadhams@valtech.co.uk>
2017-06-23 14:25:25 +0100

    Resolved error in merge

--- src/SFA.DAS.Events.Infrastructure/Data/EventsBaseRepository.cs
+++ src/SFA.DAS.Events.Infrastructure/Data/EventsBaseRepository.cs
@@ -25,66 +25,6 @@ namespace SFA.DAS.Events.Infrastructure.Data
             _connectionString = databaseConnectionString;
         }
 
-        protected async Task<T> WithConnection<T>(Func<SqlConnection, Task<T>> getData)
-        {
-            try
-            {
-                using (var connection = new SqlConnection(_connectionString))
-                {
-                    await connection.OpenAsync();
-                    return await getData(connection);
-                }
-            }
-            catch (TimeoutException ex)
-            {
-                throw new Exception($"{GetType().FullName}.WithConnection() experienced a timeout", ex);
-            }
-            catch (SqlException ex)
-            {
-                if (ex.Number == -2) // SQL Server error number for connection timeout
-                    throw new Exception($"{GetType().FullName}.WithConnection() experienced a SQL timeout", ex);
-
-                throw new Exception($"{GetType().FullName}.WithConnection() experienced a SQL exception (error code {ex.Number})", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"{GetType().FullName}.WithConnection() experienced an exception (not a SQL Exception)", ex);
-            }
-        }
-
-        protected async Task WithTransaction(Func<IDbConnection, IDbTransaction, Task> command)
-        {
-            try
-            {
-                using (var connection = new SqlConnection(_connectionString))
-                {
-                    await connection.OpenAsync();
-                    using (var trans = connection.BeginTransaction())
-                    {
-                        await command(connection, trans);
-                        trans.Commit();
-                    }
-                }
-            }
-            catch (TimeoutException ex)
-            {
-                throw new Exception($"{GetType().FullName}.WithConnection() experienced a SQL timeout", ex);
-            }
-            catch (SqlException ex) when (_transientErrorNumbers.Contains(ex.Number))
-            {
-                throw new Exception($"{GetType().FullName}.WithConnection() experienced a transient SQL Exception. ErrorNumber {ex.Number}", ex);
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception($"{GetType().FullName}.WithConnection() experienced a non-transient SQL exception (error code {ex.Number})", ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(
-                    $"{GetType().FullName}.WithConnection() experienced an exception (not a SQL Exception)", ex);
-            }
-        }
-
         protected async Task<IEnumerable<T>> GetByRange<T>(DateTime fromDate, DateTime toDate, int pageSize, int pageNumber, long fromEventId) where T : BaseEvent
         {
             var offset = pageSize*(pageNumber - 1);
