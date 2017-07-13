eb13480fb76c775ff1c930e9a29c6906154c837d
froberg <christofer.froberg@gmail.com>
2017-07-13 15:31:27 +0100

    Added events Client and Types to this solution

---
+++ src/SFA.DAS.Events.Api.Types/AgreementEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SFA.DAS.Events.Api.Types
+{
+    public class AgreementEvent
+    {
+        public string Event { get; set; }
+        public string ProviderId { get; set; }
+        public string ContractType { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Events.Api.Types/TrainingTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Events.Api.Types
+{
+    public enum TrainingTypes
+    {
+        Framework = 0,
+        Standard = 1
+    }
+}
