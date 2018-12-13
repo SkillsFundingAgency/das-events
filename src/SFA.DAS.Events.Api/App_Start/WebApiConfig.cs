1a5fe59a40976990ca07536015332f05f25aa8a0
Neil Winstanley <Winsto@users.noreply.github.com>
2018-12-13 18:21:27 +0000

    Remove Jwt token bits.

--- src/SFA.DAS.Events.Api/App_Start/WebApiConfig.cs
+++ src/SFA.DAS.Events.Api/App_Start/WebApiConfig.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
-using Microsoft.Azure;
-using SFA.DAS.ApiTokens.Client;
 
 namespace SFA.DAS.Events.Api
 {
@@ -11,12 +8,6 @@ namespace SFA.DAS.Events.Api
         public static void Register(HttpConfiguration config)
         {
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new StrictEnumConverter());
-
-            var apiKeySecret = CloudConfigurationManager.GetSetting("ApiTokenSecret");
-            var apiIssuer = CloudConfigurationManager.GetSetting("ApiIssuer");
-            var apiAudiences = CloudConfigurationManager.GetSetting("ApiAudiences").Split(' ');
-
-            config.MessageHandlers.Add(new ApiKeyHandler("Authorization", apiKeySecret, apiIssuer, apiAudiences));
 
             config.MapHttpAttributeRoutes();
 
