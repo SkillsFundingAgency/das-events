863fa5631262e5838a4626e64e48918af9ee7ac0
Neil Winstanley <Winsto@users.noreply.github.com>
2018-12-13 18:16:35 +0000

    Additions needed for AzureAD auth

---
+++ src/SFA.DAS.Events.Api/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SFA.DAS.Events.Api.Startup))]
+
+namespace SFA.DAS.Events.Api 
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
